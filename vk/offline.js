8bf884f95ae2d29dcd947f81c083a9bc5585038d
danila11246 <bezmolvnij.gamer@gmail.com>
2017-03-26 19:38:19 +0300

    Add files via upload

---
+++ vk/offline.js
@@ -0,0 +1,32 @@
+﻿{
+	"version": 1490543780,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"images/player1-sheet0.png",
+		"images/player1-sheet1.png",
+		"images/player1-sheet2.png",
+		"images/player1-sheet3.png",
+		"images/player1-sheet4.png",
+		"images/player1-sheet5.png",
+		"images/player1-sheet6.png",
+		"images/player1-sheet7.png",
+		"images/player1-sheet8.png",
+		"images/player1-sheet9.png",
+		"images/player1-sheet10.png",
+		"images/player1-sheet11.png",
+		"images/loaders-sheet0.png",
+		"images/info.png",
+		"images/fon-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"Photon-Javascript_SDK.min.js",
+		"Photon-Javascript_SDK.min.js",
+		"xd_connection.js"
+	]
+}

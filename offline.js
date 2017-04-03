bda1f6efa60445496b53c4ea6d5c5e4101634057
danila11246 <bezmolvnij.gamer@gmail.com>
2017-04-03 16:41:19 +0300

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,25 @@
+﻿{
+	"version": 1491226385,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"images/player1-sheet0.png",
+		"images/player1-sheet1.png",
+		"images/player1-sheet2.png",
+		"images/player1-sheet3.png",
+		"images/ten.png",
+		"images/icon-sheet0.png",
+		"images/tablo-sheet0.png",
+		"images/fon-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"Photon-Javascript_SDK.min.js",
+		"Photon-Javascript_SDK.min.js",
+		"xd_connection.js"
+	]
+}

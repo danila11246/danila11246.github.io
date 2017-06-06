3982e7b7df9d017a5fcaf532395c875c36063fb7
danila11246 <bezmolvnij.gamer@gmail.com>
2017-06-06 19:04:03 +0300

    Add files via upload

---
+++ test/offline.js
@@ -0,0 +1,20 @@
+﻿{
+	"version": 1496764661,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/backgroundtile.png",
+		"images/player-sheet0.png",
+		"images/solidtile.png",
+		"images/jumpthrutile.png",
+		"images/ggggggg-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
